363aa099206a6816a9f02271e781ba7c4ddd3b27
Andrei Dmitrenko <dmitrav@inbox.ru>
2020-03-12 20:23:40 +0100

    projects 1a, 1b: v.1

---
+++ 1/linear_transform.py
@@ -0,0 +1,53 @@
+
+import pandas, numpy
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+
+    path = "/Users/dmitrav/ETH/courses/iml-tasks/1/data/train_1b.csv"
+
+    with open(path) as file:
+        data = pandas.read_csv(file)
+
+    y = data.iloc[:,1].values
+    X_linear = data.iloc[:,2:].values
+    X_quadratic = numpy.power(X_linear, 2)
+    X_exponential = numpy.exp(X_linear)
+    X_cosine = numpy.cos(X_linear)
+    X_constant = numpy.full(X_linear.shape, 1)
+
+    model = LinearRegression()
+
+    coefs = []
+
+    model.fit(X_linear, y)
+    coefs.extend(model.coef_)
+    print("linear r2:", model.score(X_linear, y))
+    print("coefs:", model.coef_, "\n")
+
+    model.fit(X_quadratic, y)
+    coefs.extend(model.coef_)
+    print("quadratic r2:", model.score(X_quadratic, y))
+    print("coefs:", model.coef_, "\n")
+
+    model.fit(X_exponential, y)
+    coefs.extend(model.coef_)
+    print("exponential r2:", model.score(X_exponential, y))
+    print("coefs:", model.coef_, "\n")
+
+    model.fit(X_cosine, y)
+    coefs.extend(model.coef_)
+    print("cosine r2:", model.score(X_cosine, y))
+    print("coefs:", model.coef_, "\n")
+
+    model.fit(X_constant, y)
+    coefs.append(0.)
+    print("constant r2:", model.score(X_constant, y))
+    print("coefs:", model.coef_)
+
+    output = "\n".join([str(coef) for coef in coefs])
+
+    with open(path.replace("data/train_1b", "res/submission_1b"), 'w') as file:
+        file.write(output)
+
---
+++ 1/ridge_10_fold.py
@@ -0,0 +1,30 @@
+
+import pandas, numpy
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.linear_model import Ridge
+
+if __name__ == "__main__":
+
+    path = "/Users/dmitrav/ETH/courses/iml-tasks/1/data/train_1a.csv"
+
+    with open(path) as file:
+        data = pandas.read_csv(file)
+
+    y = data.iloc[:,1]
+    X = data.iloc[:,2:]
+
+    kf = KFold(n_splits=10)
+
+    rmse = []
+
+    for alpha in [0.01, 0.1, 1, 10, 100]:
+
+        model = Ridge(alpha=alpha)
+        cv = KFold(n_splits=10, random_state=42)
+        rmse.append(numpy.mean(cross_val_score(model, X, y, cv=10)))
+
+    output = "\n".join([str(score) for score in rmse])
+
+    with open(path.replace("data/train_1a", "res/submission_1a"), 'w') as file:
+        file.write(output)
+
