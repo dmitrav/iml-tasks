7b781b7b2965da9e9ce63ac4d30f758f9ff6af89
dmitrav <dmitrav@inbox.ru>
2020-03-25 23:44:38 +0100

    v14: sum of mse score, not rmse

--- 1/ridge_10_fold.py
+++ 1/ridge_10_fold.py
@@ -3,13 +3,13 @@ import pandas, numpy
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.metrics import make_scorer, mean_squared_error
 
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, RidgeCV
 
-script_version = "v3"
+script_version = "v4"
 
 if __name__ == "__main__":
 
-    path = "/Users/dmitrav/ETH/courses/iml-tasks/1/data/train_1a.csv"
+    path = "/Users/andreidm/ETH/courses/iml-tasks/1/data/train_1a.csv"
 
     with open(path) as file:
         data = pandas.read_csv(file)
@@ -17,15 +17,18 @@ if __name__ == "__main__":
     y = data.iloc[:,1]
     X = data.iloc[:,2:]
 
-    cv = KFold(n_splits=10, shuffle=True, random_state=42)
+    cv = KFold(n_splits=10, shuffle=True, random_state=111)
 
     rmse = []
 
     for alpha in [0.01, 0.1, 1, 10, 100]:
 
-        model = Ridge(alpha=alpha)
+        model = Ridge(alpha=alpha, fit_intercept=False)
 
-        score = numpy.sqrt(-numpy.mean(cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv)))
+        model.fit(X, y)
+
+        # score = numpy.sqrt(-numpy.mean(cross_val_score(model, X, y, scoring='neg_mean_squared_error', cv=cv)))
+        score = numpy.sqrt(mean_squared_error(y, model.predict(X)))
 
         print("alpha: ", alpha, ", score: ", score, sep="")
 
@@ -33,6 +36,6 @@ if __name__ == "__main__":
 
     output = "\n".join([str(score) for score in rmse])
 
-    with open(path.replace("data/train_1a", "res/submission_1a"), 'w') as file:
+    with open(path.replace("data/train_1a", "res/submission_1a_"+script_version), 'w') as file:
         file.write(output)
 
